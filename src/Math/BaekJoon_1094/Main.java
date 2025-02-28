a776eb2c2fbda9847d940bcbfcf62d4827de10d4
SJBaek <2021E7373@gs.anyang.ac.kr>
2025-02-28 23:58:24 +0900

    2025.02.28

---
+++ src/Math/BaekJoon_1094/Main.java
@@ -0,0 +1,11 @@
+package Math.BaekJoon_1094;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+    }
+}
